d49646d73604b7083c9b1f4138ab3b4c33c8bf1b
styputpro <styput1995@gmail.com>
2018-10-07 16:21:51 +0700

    Updated

--- app/Models/Nhanvien.php
+++ app/Models/Nhanvien.php
@@ -4,28 +4,24 @@ namespace App\Models;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use App\Notifications\ResetPassword;
 
 class Nhanvien extends Authenticatable
 {
     use Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
     protected $table = 'nhanvien';
     
     protected $fillable = [
         'name', 'email', 'password',
     ];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function sendPasswordResetNotification($token)
+    {
+      $this->notify(new ResetPassword($token));
+    }
 }
